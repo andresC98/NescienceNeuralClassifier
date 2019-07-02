2cfa11eb2f684e8e3dfae64e6843c40f81dc22ac
Andres Carrillo Lopez <acarrillolopez98@gmail.com>
2019-07-02 13:57:12 +0200

    solved grammar issue

--- NNC-GE/PonyGE2/src/fitness/NescNNclasGE.py
+++ NNC-GE/PonyGE2/src/fitness/NescNNclasGE.py
@@ -87,7 +87,7 @@ class NescNNclasGE(base_ff):
     def evaluate(self, ind, **kwargs):
 
         print(ind.phenotype)
-        eval(ind.phenotype) #self.viu, msdX, self.nn initialized here
+        exec(ind.phenotype) #self.viu, msdX, self.nn initialized here
         print("Variables in use: {}.".format(self.viu))
         #print("Layers being used: ", self.nn.layers) #TODO: make print stm more verbose
         print("msdX shape: {}.".format(msdX.shape[1]))
